6a541691e3ded87a69e714b77f35a6b65525c5ab
Ehis <ehisjude420@gmail.com>
2021-03-04 19:00:31 +0100

    if else statement

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+
+            if(age< 11)
+            {
+                Console.WriteLine("You are a child");
+            }
+
+            else if(age < 18)
+            {
+                Console.WriteLine("You are an adult");
+            }
+
+            else
+            {
+                Console.WriteLine("you are an old man");
+            }
+        }
+    }
+}
